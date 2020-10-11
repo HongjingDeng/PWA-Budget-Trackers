8a59410d7c03329bfa5fc1f8addbbb5ed358a487
Hongjing <hongjingdeng@gmail.com>
2020-10-10 21:32:04 -0400

    updating package-lock.json and date key in transactionSchema

--- models/transaction.js
+++ models/transaction.js
@@ -9,17 +9,18 @@ const transactionSchema = new Schema(
       trim: true,
       required: "Enter a name for transaction"
     },
-    value: {
-      type: Number,
-      required: "Enter an amount"
-    },
     date: {
       type: Date,
       default: Date.now
+    },
+    value: {
+      type: Number,
+      required: "Enter an amount"
     }
   }
 );
 
 const Transaction = mongoose.model("Transaction", transactionSchema);
 
+//export Transaction module
 module.exports = Transaction;
